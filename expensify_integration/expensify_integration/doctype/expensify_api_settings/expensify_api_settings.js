8801913c1266e7063c6ed6c4a8faba96d0bd9286
Uriel <urieltan3@gmail.com>
2021-05-10 07:05:54 +0000

    Edit fixtures

---
+++ expensify_integration/expensify_integration/doctype/expensify_api_settings/expensify_api_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, Ion and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Expensify API Settings', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ expensify_integration/expensify_integration/doctype/expensify_settings/expensify_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, Ion and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Expensify settings', {
+	// refresh: function(frm) {
+
+	// }
+});
