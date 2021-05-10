352d53bff6d725c54704a0e84ab6e7049170b78c
Uriel <urieltan3@gmail.com>
2021-05-10 07:57:21 +0000

    Add doctype

--- expensify_integration/expensify_integration/doctype/expensify_settings/expensify_settings.js
+++ expensify_integration/expensify_integration/doctype/expensify_settings/expensify_settings.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2021, Ion and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Expensify settings', {
+frappe.ui.form.on('Expensify Settings', {
 	// refresh: function(frm) {
 
 	// }
